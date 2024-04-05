3977b3a1123fd2089eda6497bcfa8236cf8e1edb
217023 <217023@KB37-1-120-C10>
2024-04-05 13:51:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static bool tst(int bal, int tsts)
+        {
+            switch (tsts)
+            {
+                case 1 when bal <= 10 && bal >= 0:
+                    return false;
+                case 2 when bal <= 50 && bal >= 0:
+                    return false;
+                case 3 when bal <= 30 && bal >= 0:
+                    return false;
+                case 4 when bal <= 10 && bal >= 0:
+                    return false;
+            }
+            return true;
+        }
+        public static int answ(int bal,int q=1)
+        {
+            switch (q)
+            {
+                case 1 when bal <= 100 && bal >= 70:
+                    return 5;
+                case 1 when bal <= 69 && bal >= 40:
+                    return 4;
+                case 1 when bal <= 39 && bal >= 20:
+                    return 3;
+                case 1 when bal <= 19 && bal >= 0:
+                    return 2;
+            }
+            return 6;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int one = 0; 
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine($"Введите количество баллов за {i} задание");
+                one = int.Parse(Console.ReadLine());
+                while (tst(one, i))
+                {
+                    Console.WriteLine($"Введите количество баллов за {i} задание( в 1-ом до 10, во 2-ом до 50, в 3-ем до 30, в 4-ом до 10 )");
+                    one = int.Parse(Console.ReadLine());
+                }
+                sum += one;
+            }
+            Console.WriteLine($"Набранное количество баллов: {sum}, оценка {answ(sum)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static ConsoleApp1.Program;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(tst(50, 1));
+            Assert.IsFalse(tst(10, 1));
+            Assert.AreEqual(answ(50), 4);
+            Assert.IsTrue(tst(10, 1));
+            Assert.IsFalse(tst(59, 1));
+            Assert.AreEqual(answ(50), 5);
+        }
+    }
+}
